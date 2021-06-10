629c55627f70416168019adf01f131cf00eab365
junaiyet <junaiyetmia470@gmail.com>
2021-06-10 21:10:56 +0600

    tril project

--- asset/js/script.js
+++ asset/js/script.js
@@ -40,3 +40,43 @@ new WOW().init();
       // instead of a settings object
     ]
   });
+ 
+ $('.properties-slider').slick({
+    dots: false,
+    infinite: true,
+    arrows:true,
+    prevArrow:'<i class="fa fa-long-arrow-left prev" aria-hidden="true"></i>',
+    nextArrow:'<i class="fa fa-long-arrow-right next" aria-hidden="true"></i>',
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+   
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
