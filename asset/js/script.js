62e6ac6ee6bf7a59416a15f7fb7ab735ba8d9b5b
junaiyet <junaiyetmia470@gmail.com>
2021-06-11 03:12:04 +0600

    tril project

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,22 @@
 new WOW().init();
  
  
+
+$(".backtotop").click(function(){
+ $("html,body").animate ({
+   scrollTop:0,
+ },1000)
+});
+
+$(window).scroll(function(){
+if($(window).scrollTop() > 800){
+  $(".backtotop").fadeIn()
+}else{
+  $(".backtotop").fadeOut()
+}
+});
+
+
  $('.banner-slider').slick({
     dots: false,
     infinite: true,
@@ -51,6 +67,89 @@ new WOW().init();
     slidesToShow: 4,
     slidesToScroll: 1,
    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+ $('.client-slider').slick({
+    dots: false,
+    infinite: true,
+    arrows:true,
+    prevArrow:'<i class="fa fa-long-arrow-left prev" aria-hidden="true"></i>',
+    nextArrow:'<i class="fa fa-long-arrow-right next" aria-hidden="true"></i>',
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+   
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+ 
+ 
+ 
+ 
+  $('.blog-slider').slick({
+    dots: false,
+    infinite: true,
+    arrows:true,
+    prevArrow:'<i class="fa fa-long-arrow-left prev" aria-hidden="true"></i>',
+    nextArrow:'<i class="fa fa-long-arrow-right next" aria-hidden="true"></i>',
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode:true,
+    centerPadding:"0",
     responsive: [
       {
         breakpoint: 1024,
